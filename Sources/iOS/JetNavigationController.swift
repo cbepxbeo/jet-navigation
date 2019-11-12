b4e7e371444e155cffda688b5e6b0f3ce6f8e0fb
cbepxbeo <cbepxbeo@mail.ru>
2019-11-12 16:55:47 +0300

    JetNavigationController

---
+++ Sources/iOS/JetNavigationController.swift
@@ -0,0 +1,75 @@
+//
+// Project: JetNavigation
+// File: JetNavigationController.swift
+// Created by: Egor Boyko
+// Date: 12.11.2019
+//
+// Status: #In progress | #Not decorated
+//
+
+
+import SwiftUI
+
+public final class JetNavigationController: ObservableObject{
+    
+    @Published internal var current: JetNavigationViewWrapper?
+    @Published private(set) var option: JetNavigationOption
+    
+    private let easing: Animation
+    
+    private var storage: JetNavigationStorage {
+        didSet {
+            withAnimation(self.easing) {
+                self.current = self.storage.last
+            }
+        }
+    }
+    
+    public init(_ easing: Animation, option: JetNavigationOption = .forward){
+        self.easing = easing
+        self.option = option
+        self.storage = .init()
+    }
+    
+    //MARK: Public methods
+    
+    ///Return to the root view
+    /// - Parameter option: transition option (forward/backward)
+    public func home(option: JetNavigationOption = .backward){
+        self.option = option
+        self.storage.removeAll()
+    }
+    
+    ///Go back one screen
+    /// - Parameter option: transition option (forward/backward)
+    public func back(option: JetNavigationOption = .backward){
+        self.option = option
+        self.storage.removeLast()
+    }
+    
+    ///Go to assigned screen
+    /// - Parameter representation: Destination View
+    /// - Parameter tag: unique tag for Destination Screen
+    /// - Parameter option: transition option (forward/backward)
+    public func goTo<Representation: View>(
+        representation: Representation,
+        tag: String = UUID().uuidString,
+        option: JetNavigationOption = .forward
+    )
+    {
+        self.option = option
+        self.storage.append(
+            JetNavigationViewWrapper(
+                id: tag,
+                view: AnyView(representation)
+            )
+        )
+    }
+    
+    ///Go to the presented screen by tag
+    /// - Parameter tag: screen tag
+    /// - Parameter option: transition option (forward/backward)
+    public func goTo(tag: String, option: JetNavigationOption = .backward){
+        self.storage.move(tag: tag)
+    }
+}
