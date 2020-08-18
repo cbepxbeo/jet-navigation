6c6a81438e4d3a1e6a08a24716e3b21d33e250e7
Егор Олегович <cbepxbeo@mail.ru>
2020-08-18 09:02:00 +0300

    JetNavigationButton file

---
+++ Sources/iOS/JetNavigationButton.swift
@@ -0,0 +1,14 @@
+//
+// Project: JetNavigation
+// File: JetNavigationButton.swift
+// Created by: Egor Boyko
+// Date: 18.08.2020
+//
+// Status: #In progress | #Not decorated
+//
+
+
+/**
+ The base type that conforms to the button style protocol. Required to initialize a link without specifying a style, without the need for further use.
+*/
+public struct JetNavigationButton {}
