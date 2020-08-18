85413332ab133c0ffb421f973d8e38c35e3b9978
Егор Олегович <cbepxbeo@mail.ru>
2020-08-18 09:02:05 +0300

    JetNavigationButton code

--- Sources/iOS/JetNavigationButton.swift
+++ Sources/iOS/JetNavigationButton.swift
@@ -4,11 +4,15 @@
 // Created by: Egor Boyko
 // Date: 18.08.2020
 //
-// Status: #In progress | #Not decorated
+// Status: #Completed | #Decorated
 //
 
 
 /**
  The base type that conforms to the button style protocol. Required to initialize a link without specifying a style, without the need for further use.
 */
-public struct JetNavigationButton {}
+public struct JetNavigationButton : ButtonStyle {
+    public func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+    }
+}
